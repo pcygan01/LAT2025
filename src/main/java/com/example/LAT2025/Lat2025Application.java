c825e06b0ebf5a37c509a2c2a1dd4f4d24107064
pcygan <piotr.cygan01@interia.pl>
2025-05-11 02:38:33 +0200

    init

---
+++ src/main/java/com/example/LAT2025/Lat2025Application.java
@@ -0,0 +1,13 @@
+package com.example.LAT2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lat2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lat2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LAT2025/Lat2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LAT2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lat2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
